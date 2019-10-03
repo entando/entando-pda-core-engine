d0b305beb4e717ac31cd31a1866d4e08c8b085ee
Filipe Leandro <filipeleandro87@gmail.com>
2019-10-03 10:36:24 -0300

    PDA5 reversing accidental commit

--- src/main/java/org/entando/plugins/pda/core/service/task/TaskService.java
+++ src/main/java/org/entando/plugins/pda/core/service/task/TaskService.java
@@ -1,13 +1,12 @@
 package org.entando.plugins.pda.core.service.task;
 
-import org.entando.keycloak.security.AuthenticatedUser;
 import org.entando.plugins.pda.core.engine.Connection;
 import org.entando.plugins.pda.core.model.Task;
 import org.entando.web.request.PagedListRequest;
 import org.entando.web.response.PagedRestResponse;
 
 public interface TaskService {
-    PagedRestResponse<Task> list(Connection connection, AuthenticatedUser user, PagedListRequest restListRequest);
+    PagedRestResponse<Task> list(Connection connection, PagedListRequest restListRequest);
 
-    Task get(Connection connection, AuthenticatedUser user, String id);
+    Task get(Connection connection, String id);
 }
