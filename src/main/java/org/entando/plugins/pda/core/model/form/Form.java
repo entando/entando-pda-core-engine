f35eccae1622a8e84846a0fffc574576f2a64a42
Filipe Leandro <filipeleandro87@gmail.com>
2020-01-23 18:52:00 -0300

    PDA-17 Added endpoints to submit a new process form; Added support to SubForm field types;

--- src/main/java/org/entando/plugins/pda/core/model/form/Form.java
+++ src/main/java/org/entando/plugins/pda/core/model/form/Form.java
@@ -3,6 +3,7 @@ package org.entando.plugins.pda.core.model.form;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Singular;
 
 @Data
 @Builder
@@ -10,5 +11,18 @@ public class Form {
 
     private String id;
     private String name;
+    private String type;
+
+    @Singular
     private List<FormField> fields;
+
+    public FormField getFieldByName(String id) {
+        for (FormField field : fields) {
+            if (id.equalsIgnoreCase(field.getName())) {
+                return field;
+            }
+        }
+
+        return null;
+    }
 }
--- src/main/java/org/entando/plugins/pda/core/service/task/TaskFormService.java
+++ src/main/java/org/entando/plugins/pda/core/service/task/TaskFormService.java
@@ -1,16 +1,15 @@
 package org.entando.plugins.pda.core.service.task;
 
-import java.util.List;
+import java.util.Map;
 import org.entando.keycloak.security.AuthenticatedUser;
 import org.entando.plugins.pda.core.engine.Connection;
-import org.entando.plugins.pda.core.model.Task;
 import org.entando.plugins.pda.core.model.form.Form;
-import org.entando.plugins.pda.core.model.task.CreateTaskFormSubmissionRequest;
 
 public interface TaskFormService {
 
-    List<Form> get(Connection connection, String taskId);
+    Form get(Connection connection, String taskId);
 
-    Task submit(Connection connection, AuthenticatedUser user, String taskId, CreateTaskFormSubmissionRequest request);
+    String submit(Connection connection, AuthenticatedUser user, String taskId,
+            Map<String, Object> request);
 
 }
