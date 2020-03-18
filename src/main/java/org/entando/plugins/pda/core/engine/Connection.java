df42a4a07e9aa53c3631becc20ce08b1d5e34bb7
filipeleandro87@gmail.com <ffleandro>
2020-03-18 16:54:59 -0300

    PDA-164 changed connection format

--- src/main/java/org/entando/plugins/pda/core/engine/Connection.java
+++ src/main/java/org/entando/plugins/pda/core/engine/Connection.java
@@ -13,23 +13,11 @@ import lombok.NoArgsConstructor;
 public class Connection {
 
     private String name;
-    private String host;
-    private String port;
-    private String schema;
-    private String app;
+    private String url;
     private String username;
     private String password;
     private Integer connectionTimeout;
     private Map<String, String> properties;
     private String engine;
 
-    public String getUrl() {
-        String url = String.format("%s://%s:%s%s", schema, host, port, app);
-
-        if (url.substring(url.length() - 1).equals("/")) {
-            url = url.substring(0, url.length() - 1);
-        }
-
-        return url;
-    }
 }
