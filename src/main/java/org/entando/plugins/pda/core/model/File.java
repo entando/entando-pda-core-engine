a5aaba90dc7ab4de3d455a54dd762e018678fd65
Filipe Leandro <ffleandro>
2020-03-27 15:04:08 -0300

    PDA-171 cleaning up

--- src/main/java/org/entando/plugins/pda/core/model/File.java
+++ src/main/java/org/entando/plugins/pda/core/model/File.java
@@ -1,5 +1,7 @@
 package org.entando.plugins.pda.core.model;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,6 +10,7 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
+@SuppressFBWarnings({ "EI_EXPOSE_REP", "EI_EXPOSE_REP2" })
 public class File {
     private String type;
     private String name;
@@ -24,7 +27,7 @@ public class File {
             } else if (property.startsWith("name=")) {
                 name = property.replace("name=", "").replace("%20", " ");
             } else if (property.startsWith("base64,")) {
-                data = property.replace("base64,", "").getBytes();
+                data = property.replace("base64,", "").getBytes(StandardCharsets.UTF_8);
                 size = Base64.getDecoder().decode(data).length;
             }
         }
