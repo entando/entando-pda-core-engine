1b120a46793027abf0440464d2d70b38ec951804
Filipe Leandro <ffleandro>
2020-04-01 11:49:43 -0300

    PDA-168 implemented tests to cover full task form sample;

--- src/test/java/org/entando/plugins/pda/core/service/task/FakeTaskFormService.java
+++ src/test/java/org/entando/plugins/pda/core/service/task/FakeTaskFormService.java
@@ -20,6 +20,8 @@ import static org.entando.plugins.pda.core.utils.TestUtils.TASK_ID_1;
 import static org.entando.plugins.pda.core.utils.TestUtils.TASK_ID_2;
 import static org.entando.plugins.pda.core.utils.TestUtils.TASK_NAME_1;
 import static org.entando.plugins.pda.core.utils.TestUtils.TASK_NAME_2;
+import static org.entando.plugins.pda.core.utils.TestUtils.createFullTaskForm;
+import static org.entando.plugins.pda.core.utils.TestUtils.createSimpleTaskForm;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -45,65 +47,15 @@ public class FakeTaskFormService implements TaskFormService {
 
     private final FakeTaskService taskService;
 
-    public static final Form TASK_FORM_1;
-
-    static {
-        FormField formField11 = FormFieldNumber.builder()
-                .id(TASK_FORM_PROP_1)
-                .type(FormFieldType.INTEGER)
-                .name(TASK_FORM_PROP_KEY_1)
-                .label(TASK_FORM_PROP_1)
-                .placeholder(TASK_FORM_PROP_DESCRIPTION_1)
-                .minValue(0.0)
-                .maxValue(10.0)
-                .build();
-
-        FormField formField12 = FormField.builder()
-                .id(TASK_FORM_PROP_2)
-                .type(FormFieldType.BOOLEAN)
-                .name(TASK_FORM_PROP_KEY_2)
-                .label(TASK_FORM_PROP_2)
-                .placeholder(TASK_FORM_PROP_DESCRIPTION_2)
-                .build();
-
-        FormFieldSubForm formField13 = FormFieldSubForm.builder()
-                .id(TASK_FORM_PROP_3)
-                .type(FormFieldType.SUBFORM)
-                .name(TASK_FORM_PROP_KEY_3)
-                .label(TASK_FORM_PROP_3)
-                .placeholder(TASK_FORM_PROP_DESCRIPTION_3)
-                .formId(TASK_FORM_ID_2)
-                .formType(TASK_FORM_TYPE_2)
-                .build();
-
-        FormField formField21 = FormFieldText.builder()
-                .id(TASK_FORM_PROP_4)
-                .type(FormFieldType.STRING)
-                .name(TASK_FORM_PROP_KEY_4)
-                .label(TASK_FORM_PROP_4)
-                .placeholder(TASK_FORM_PROP_DESCRIPTION_4)
-                .build();
-
-        TASK_FORM_1 = Form.builder()
-                .id(TASK_FORM_ID_1)
-                .name(TASK_NAME_1)
-                .type(TASK_FORM_TYPE_1)
-                .fields(Arrays.asList(formField11, formField12, formField13))
-                .build();
-
-        Form subForm = Form.builder()
-                .id(TASK_ID_2)
-                .name(TASK_NAME_2)
-                .fields(Collections.singletonList(formField21))
-                .build();
-
-        formField13.setForm(subForm);
-    }
+    public static final Form TASK_FORM_1 = createSimpleTaskForm();
+    public static final Form TASK_FORM_2 = createFullTaskForm();
 
     @Override
     public Form get(Connection connection, String taskId) {
         if (TASK_ID_1.equals(taskId)){
             return TASK_FORM_1;
+        } else if (TASK_ID_2.equals(taskId)){
+            return TASK_FORM_2;
         }
 
         throw new TaskNotFoundException();
